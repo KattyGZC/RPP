62c690f9be990358a590e27de468cda17cf2b438
KattyGZC <kattyko.gzc@gmail.com>
2020-06-24 21:13:58 -0500

    Vista signin

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.signin, name='signin'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from .models import  Profile, Level, Exercise, Score
 
 
-def index(request):
-    #return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'index.html')
+# def index(request):
+#     #return HttpResponse("Hello, world. You're at the polls index.")
+#     return render(request, 'index.html')
+
+def signin(request):
+    return render(request, 'sign_in.html')
